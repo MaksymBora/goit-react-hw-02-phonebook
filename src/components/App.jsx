d77c66247646630aa2a270c8703ec2b98ece8ce6
MaksymBora <maxboraod@gmail.com>
2023-08-08 23:28:46 +0200

    add filter input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,13 @@ export class App extends Component {
     super(props);
 
     this.state = {
-      contacts: [],
+      contacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      ],
+      filter: '',
       name: '',
       number: ''
     };
@@ -74,7 +80,11 @@ export class App extends Component {
 
             <button type="submit">Add Contanct</button>
           </form>
-          <p>{this.contacts}</p>
+
+          <p>Find contacts by name</p>
+          <input
+            type="text"
+            placeholder="Filter contacts"/>
           <h2>Contacts</h2>
           <ul>
             {this.state.contacts.map(contact => {
