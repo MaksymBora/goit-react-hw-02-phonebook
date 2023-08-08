9703ffd44539e53b2645d20cfee5a96ffa97905e
MaksymBora <maxboraod@gmail.com>
2023-08-08 23:24:06 +0200

    add number

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,23 +17,30 @@ export class App extends Component {
     this.state = {
       contacts: [],
       name: '',
+      number: ''
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeNumber = this.handleChangeNumber.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   
-  handleChange(event) {
+  handleChangeName(event) {
     this.setState({name: event.target.value});
+  }
+
+    handleChangeNumber(event) {
+    this.setState({number: event.target.value});
   }
 
   handleSubmit = event => {
     event.preventDefault();
     
-    let newContact = { name: this.state.name, id: nanoid() };
+    let newContact = { name: this.state.name, id: nanoid(), number: this.state.number };
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
       name: '',
+      number: '',
     }));
    
 
@@ -48,12 +55,23 @@ export class App extends Component {
             <label>Name</label>
             <input
               value={this.state.name}
-              onChange={this.handleChange}
+              onChange={this.handleChangeName}
               type="text"
               name="name"
               pattern="^[a-zA-Z'\- ]+$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required />
+            
+            <input
+               value={this.state.number}
+              onChange={this.handleChangeNumber}
+              type="tel"
+              name="number"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              title="073-000-0000"
+              required
+            />
+
             <button type="submit">Add Contanct</button>
           </form>
           <p>{this.contacts}</p>
@@ -61,7 +79,7 @@ export class App extends Component {
           <ul>
             {this.state.contacts.map(contact => {
               return (
-                    <li key={contact.id}>{contact.name}</li>
+                <li key={contact.id}>{contact.name}: { contact.number}</li>
                  )
               })}
             
