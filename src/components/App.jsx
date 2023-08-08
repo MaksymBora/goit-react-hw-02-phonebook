cadaf8926c1a13e7b9a1d81a41c14a56221eadaa
MaksymBora <maxboraod@gmail.com>
2023-08-08 21:55:42 +0200

    add markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { nanoid } from 'nanoid'
+
+import { Component } from 'react';
+
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  }
+  
+  render() {
+    return (
+      <>
+        <div>
+          <form>
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Z-' ]+$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required />
+            <button>Add Contanct</button>
+          </form>
+
+          <h2>Contacts</h2>
+          <ul>
+            <li>
+            </li>
+          </ul>
+        </div>
+    </>)
+    
+  }
+
 };
