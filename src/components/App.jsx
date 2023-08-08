d885585c92df63fbfd146ec689841b686ced9da6
MaksymBora <maxboraod@gmail.com>
2023-08-08 23:13:13 +0200

    add form and contact submit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,74 @@
-// import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid'
 
 import { Component } from 'react';
 
 
+// const INITIAL_STATE = {
+//   contacts: [],
+//     name: '',
+    
+// }
+
 export class App extends Component {
-  state = {
-    contacts: [],
-    name: ''
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: [],
+      name: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   
+  handleChange(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    
+    let newContact = { name: this.state.name, id: nanoid() };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+      name: '',
+    }));
+   
+
+  };
+  
+
   render() {
     return (
       <>
         <div>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <label>Name</label>
             <input
+              value={this.state.name}
+              onChange={this.handleChange}
               type="text"
               name="name"
-              pattern="^[a-zA-Z-' ]+$"
+              pattern="^[a-zA-Z'\- ]+$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required />
-            <button>Add Contanct</button>
+            <button type="submit">Add Contanct</button>
           </form>
-
+          <p>{this.contacts}</p>
           <h2>Contacts</h2>
           <ul>
-            <li>
-            </li>
+            {this.state.contacts.map(contact => {
+              return (
+                    <li key={contact.id}>{contact.name}</li>
+                 )
+              })}
+            
           </ul>
         </div>
-    </>)
+      </>
+    )
     
   }
 
