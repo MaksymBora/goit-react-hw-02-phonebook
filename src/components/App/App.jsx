91da6a41dc25b0a08c5357f7f5ccf10468c054c3
MaksymBora <maxboraod@gmail.com>
2023-08-11 18:51:42 +0200

    add rules

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,19 +8,29 @@ import { AppWrapper,Title, SearchWrapper, StyledTitles } from './app.styled';
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
       {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: ''
     };
   
   addContact = (newContact) => {
+    if (this.state.contacts.find(contact => contact.name === newContact.name)) {
+      return alert(`${newContact.name} is already in contacts`);
+    }
+      
+    if (this.state.contacts.find(contact => contact.number === newContact.number)) {
+      return alert(`${newContact.number} is already in contacts`);
+    }
+
     this.setState(prevState => ({
       contacts: [ ...prevState.contacts, newContact]
     }));
   };
+
+
 
   getContact = evt => {
     const searchQuerry = evt.currentTarget.value;
